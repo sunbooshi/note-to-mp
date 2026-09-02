fde1e7a46e4671870d0ffa14afdc1e76be125acb
sunbooshi <sunbooshi@gmail.com>
2026-09-02 09:08:42 +0800

    fix: Update CoverSearchHost URL to correct endpoint

--- src/unsplash.ts
+++ src/unsplash.ts
@@ -30,7 +30,7 @@ import { mimeToImageExt } from "./utils";
  *   GET /v1/cover/search?query=xxx&page=1&per_page=10
  *   Authorization: Bearer {authkey}
  */
-export const CoverSearchHost = "https://obsidian.dualhue.cn";
+export const CoverSearchHost = "https://obplugin.dualhue.cn";
 
 export interface UnsplashUrls {
 	thumb: string;
